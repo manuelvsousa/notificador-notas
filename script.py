6f0d94490e73e5ace2ef0ef70b0464901ba74249
Manuel Sousa <manuelsousamvs@gmail.com>
2016-07-02 22:26:18 +0100

    GOOOO

--- script.py
+++ script.py
@@ -4,6 +4,9 @@ import urllib2 #get source code
 import sys
 import ast
 import json
+import hashlib
+from hashlib import md5
+import urllib2
 
 def is_up(link):
     return urllib.urlopen(link).getcode() == 200
@@ -28,11 +31,13 @@ def ConfigSectionMap(Config,section):
 
 def get_sourcecode(link):
     response = urllib2.urlopen(link)
-    return response.read()
+    m = hashlib.md5()
+    m.update(response.read().encode('utf-8'))
+    return m.hexdigest()
 
 def config_init(name):
     config = ConfigParser.ConfigParser()
-    config.read("config.ini")
+    config.read(name)
     return config
 
 def get_cadeiras(config):
@@ -58,8 +63,36 @@ def del_reverse_list_index(data,eliminar):
     for i in reversed(eliminar):
         del(data[i])
 
+def atualizar_cadeira(data,config):
+    if(is_up(data["link"])):
+        md5=get_sourcecode(data["link"])
+        if(md5 != data["md5"]):
+            #alerta(data,config)
+            altera_config(data["link"],config)
+            del(data)
+    else:
+        error_log("IS DOWN!! " % data["link"])
 
-print is_up("http://google.com")
+def altera_config(data,config):
+    config.set(data["cadeira"], data["ativo"], False)
+    with open('config.ini', 'w') as configfile:
+        config.write(config)
+
+
+
+
+
+
+def adicionar_cadeira(cadeira,config):
+    new={}
+    lista=[]
+    new["md5"]=get_sourcecode(ConfigSectionMap(cadeira)['link'])
+    new["link"]=ConfigSectionMap(cadeira)['link']
+    new["cadeira"]=cadeira
+    new["sigla"]=ConfigSectionMap(cadeira)['sigla']
+    new["ativo"]=True
+    return new
+
 
 
 
@@ -71,20 +104,22 @@ if __name__ == "__main__":
     #eliminar jsons que ja nao existem na config
     for a in range(0,len(data)):
         for b in range(0,len(cadeiras)):
-            if cadeiras[b]==data[a]:
+            if cadeiras[b] == data[a]["cadeira"]:
                 break
         if (b+1)!=len(data):
             eliminar.append(a)
     del_reverse_list_index(data,eliminar)
     #-------------------------------------
-    adicionar=[]
+    #atualiza cadeiras e adiciona novas cadeiras
+    novos=[]
     for a in range(0,len(cadeiras)):
         for b in range(0,len(data)):
-            if cadeiras[b]==data[a]:
-                atualizar_cadeira()
+            if cadeiras[b] == data[a]["cadeira"]:
+                atualizar_cadeira(data[a],config)
         if (b+1)!=len(cadeiras):
-            adicionar_cadeira(data,cadeiras[i],config)
-
+            novos.append(adicionar_cadeira(cadeiras[i],config))
+    data=data+novos
+    #-------------------------------------
 
 
 
