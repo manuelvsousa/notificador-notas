9123146dc3aadd88db17398679e13ab4840eff19
Manuel Sousa <manuelsousamvs@gmail.com>
2017-05-07 22:41:30 +0100

    major updates

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
-import ConfigParser #Configuration files
-#import urllib #Page is up
-import urllib2 #Page is up
+import ConfigParser  # Configuration files
+# import urllib #Page is up
+import urllib2  # Page is up
 import httplib
 from urlparse import urlparse
 import json
@@ -9,9 +9,10 @@ import requests
 import time
 
 ################################################
-FICHEIRO_CONFIG="config.ini"
-FICHEIRO_DATA="data.txt"
+FICHEIRO_CONFIG = "config.ini"
+FICHEIRO_DATA = "data.txt"
 ################################################
+
 
 def is_up(url):
     try:
@@ -23,12 +24,14 @@ def is_up(url):
     except httplib.URLError:
         return False
 
+
 def error_log(message):
     file = open("error.log", "rw+")
     line = file.write(message)
     file.close()
 
-def ConfigSectionMap(Config,section):
+
+def ConfigSectionMap(Config, section):
     dict1 = {}
     options = Config.options(section)
     for option in options:
@@ -41,68 +44,77 @@ def ConfigSectionMap(Config,section):
             dict1[option] = None
     return dict1
 
+
 def get_sourcecode(link):
     response = urllib2.urlopen(link)
+    if(response.code != 200 and str(response.read()).find("open-source, academic, information, ") != -1):
+        return "error"
     m = hashlib.md5()
     m.update(response.read())
     return m.hexdigest()
+
 
 def config_init(name):
     config = ConfigParser.ConfigParser()
     config.read(name)
     return config
 
+
 def get_cadeiras(config):
-    i=0
+    i = 0
     for a in config.sections():
-        if(a!="email"):
-            i=i+1
+        if(a != "email"):
+            i = i + 1
         else:
             break
     return config.sections()[0:i]
 
 
-
 def read_file_lines(file):
     with open(file) as f:
         lines = f.readlines()
-    for i in range(0,len(lines)):
+    for i in range(0, len(lines)):
         json_acceptable_string = lines[i].replace("'", "\"")
         d = json.loads(json_acceptable_string)
-        lines[i]=d
+        lines[i] = d
     return lines
 
 
-def del_reverse_list_index(data,eliminar):
+def del_reverse_list_index(data, eliminar):
     for i in reversed(eliminar):
         del(data[i])
 
-def send_simple_message(email,cadeira_nome,cadeira_sigla,link,nome):
+
+def send_simple_message(email, cadeira_nome, cadeira_sigla, link, nome):
     return requests.post(
         "https://api.mailgun.net/v3/sandboxc8e792abe62b4e0a8807ae40829d329e.mailgun.org/messages",
         auth=("api", "key-e2c43c98f6a756838662b39bf3c93253"),
         data={"from": "Notas Tecnico <notas@tecnico.ulisboa.pt>",
               "to": nome + " <" + email + ">",
-              "subject": "Notas de " + cadeira_nome + " ("+cadeira_sigla+")",
-              "text": "As Notas de " + cadeira_nome + " ("+cadeira_sigla+") estao disponiveis em " + link})
+              "subject": "Notas de " + cadeira_nome + " (" + cadeira_sigla + ")",
+              "text": "As Notas de " + cadeira_nome + " (" + cadeira_sigla + ") estao disponiveis em " + link})
+    publish_facebook("[" + cadeira_sigla + "]\nAs Notas de " + cadeira_nome +
+                     " (" + cadeira_sigla + ") estao disponiveis em " + link)
 
 
-def atualizar_cadeira(data,config):
-    if(is_up(data["link"])):
-        md5=get_sourcecode(data["link"])
-        if(md5 != data["md5"]):
-            #alerta(data,config)
-            print "ALERTA!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-            if str2bool(ConfigSectionMap(config,"email")['ativo'])==True:
-                send_simple_message(ConfigSectionMap(config,"email")['email'],data["cadeira"],data["sigla"],data["link"],ConfigSectionMap(config,"email")['nome'])
-            altera_config(data,config)
-            return True
-        else:
-            return False
-    else:
+def atualizar_cadeira(data, config):
+    md5 = get_sourcecode(data["link"])
+    if(md5 == "error"):
         error_log("IS DOWN!! " % data["link"])
+        return False
 
-def altera_config(data,config):
+    if(md5 != data["md5"]):
+        print "Detecao alterada"
+        if str2bool(ConfigSectionMap(config, "email")['ativo']) == True:
+            send_simple_message(ConfigSectionMap(config, "email")['email'], data["cadeira"], data[
+                                    "sigla"], data["link"], ConfigSectionMap(config, "email")['nome'])
+        altera_config(data, config)
+        return True
+    else:
+        return False
+
+
+def altera_config(data, config):
     try:
         from configparser import ConfigParser
     except ImportError:
@@ -113,84 +125,92 @@ def altera_config(data,config):
     with open(FICHEIRO_CONFIG, 'w') as configfile:
         config.write(configfile)
 
-def adicionar_cadeira(cadeira,config):
-    new={}
-    lista=[]
-    new["cadeira"]=cadeira
-    new["sigla"]=ConfigSectionMap(config,cadeira)['sigla']
-    new["link"]=ConfigSectionMap(config,cadeira)['link']
-    new["ativo"]=bool(ConfigSectionMap(config,cadeira)['ativo'])
-    new["md5"]=get_sourcecode(ConfigSectionMap(config,cadeira)['link'])
+
+def adicionar_cadeira(cadeira, config):
+    new = {}
+    lista = []
+    new["cadeira"] = cadeira
+    new["sigla"] = ConfigSectionMap(config, cadeira)['sigla']
+    new["link"] = ConfigSectionMap(config, cadeira)['link']
+    new["ativo"] = bool(ConfigSectionMap(config, cadeira)['ativo'])
+    new["md5"] = get_sourcecode(ConfigSectionMap(config, cadeira)['link'])
     return json.loads(json.dumps(new))
 
+
 def record_data(data):
-    f = open(FICHEIRO_DATA,'w')
-    for i in data: 
+    f = open(FICHEIRO_DATA, 'w')
+    for i in data:
         i = json.dumps(i, ensure_ascii=False, encoding='utf8')
-        f.write(unicode(i)+"\n")
+        f.write(unicode(i) + "\n")
     f.close()
 
-def del_unexistent_json(data,cadeiras):
-    eliminar=[]
-    for a in range(0,len(data)):
-        for b in range(0,len(cadeiras)):
+
+def del_unexistent_json(data, cadeiras):
+    eliminar = []
+    for a in range(0, len(data)):
+        for b in range(0, len(cadeiras)):
             if cadeiras[b] == data[a]["cadeira"]:
                 break
-            elif (b+1)==len(cadeiras):
+            elif (b + 1) == len(cadeiras):
                 eliminar.append(a)
-    del_reverse_list_index(data,eliminar)
+    del_reverse_list_index(data, eliminar)
+
 
 def str2bool(s):
-    return s.lower() in ["true","t","1"]
+    return s.lower() in ["true", "t", "1"]
 
 
+def publish_facebook(message):
+    r = requests.post("https://graph.facebook.com/v2.9/624839094387971/feed", data={
+                      'access_token': "EAAKYjJJ6YwkBAMhYh3NVsrSRoB72s2UsG3Vy3oTssdXpHvK9Ghjah8ZBogeRkl4xCYMK3F8PE1OJ8g8MwNqoxqyO6vZB87JZB18Sq69iYKJ5sPAl6S9fQhHLNd59U9lKRYGOLG5oymqjZCCZBWEXgCZBpHWypKRjRVqWiZBeeGqZB7ZBicbNpov1tAellSGG5ZC5YZD", 'message': message})
 
-def update_add_cadeiras(cadeiras,data,config):
-    novos=[]
-    achas=[]
-    pos=[]
-    for b in range(0,len(cadeiras)):
-        if len(data)==0 and str2bool(ConfigSectionMap(config,cadeiras[b])['ativo'])==True:
-            novos.append(adicionar_cadeira(cadeiras[b],config))
-        for a in range(0,len(data)):
 
-            if cadeiras[b] == data[a]["cadeira"] and data[a]["ativo"]==True:
-                if str2bool(ConfigSectionMap(config,cadeiras[b])['ativo'])==True:
-                    if atualizar_cadeira(data[a],config):
-                        pos=pos+[a]
-                        data[a]["ativo"]=False
+def update_add_cadeiras(cadeiras, data, config):
+    novos = achas = pos = []
+    for b in range(0, len(cadeiras)):
+        if len(data) == 0 and str2bool(ConfigSectionMap(config, cadeiras[b])['ativo']) == True:
+            novos.append(adicionar_cadeira(cadeiras[b], config))
+        for a in range(0, len(data)):
+
+            if cadeiras[b] == data[a]["cadeira"] and data[a]["ativo"] == True:
+                if str2bool(ConfigSectionMap(config, cadeiras[b])['ativo']) == True:
+                    if atualizar_cadeira(data[a], config):
+                        pos = pos + [a]
+                        data[a]["ativo"] = False
                 else:
-                    pos=pos+[a]
-                achas=achas+[data[a]["cadeira"]]
-            if (a+1)==len(data) and cadeiras[b] not in achas and str2bool(ConfigSectionMap(config,cadeiras[b])['ativo'])==True:
-                novos.append(adicionar_cadeira(cadeiras[b],config))
-    data=data+novos
-    del_reverse_list_index(data,pos)
+                    pos = pos + [a]
+                achas = achas + [data[a]["cadeira"]]
+            if (a + 1) == len(data) and cadeiras[b] not in achas and str2bool(ConfigSectionMap(config, cadeiras[b])['ativo']) == True:
+                novos.append(adicionar_cadeira(cadeiras[b], config))
+    data = data + novos
+    del_reverse_list_index(data, pos)
     return data
 
 if __name__ == "__main__":
-    segundos=3
+    segundos = 3
+    publish_facebook("ola")
     while 1:
         time.sleep(segundos)
         config = config_init(FICHEIRO_CONFIG)
-        if str2bool(ConfigSectionMap(config,"running")['crontab'])==True:
-            crontab=True
+        if str2bool(ConfigSectionMap(config, "running")['crontab']) == True:
+            crontab = True
             break
-        cadeiras=get_cadeiras(config)
+        cadeiras = get_cadeiras(config)
         data = read_file_lines(FICHEIRO_DATA)
-        #eliminar jsons que ja nao existem na config WORKING TESTED
-        del_unexistent_json(data,cadeiras)
-        #atualiza cadeiras e adiciona novas cadeiras WORKING - NOT FULL TESTED
-        data=update_add_cadeiras(cadeiras,data,config)
-        #escreve no ficheiro data
+        # eliminar jsons que ja nao existem na config WORKING TESTED
+        del_unexistent_json(data, cadeiras)
+        # atualiza cadeiras e adiciona novas cadeiras WORKING - NOT FULLY
+        # TESTED
+        data = update_add_cadeiras(cadeiras, data, config)
+        # escreve no ficheiro data
         record_data(data)
-        segundos=float(ConfigSectionMap(config,"running")['segundos'])
-    if crontab==True:
-        cadeiras=get_cadeiras(config)
+        segundos = float(ConfigSectionMap(config, "running")['segundos'])
+    if crontab == True:
+        cadeiras = get_cadeiras(config)
         data = read_file_lines(FICHEIRO_DATA)
-        #eliminar jsons que ja nao existem na config WORKING TESTED
-        del_unexistent_json(data,cadeiras)
-        #atualiza cadeiras e adiciona novas cadeiras WORKING - NOT FULL TESTED
-        data=update_add_cadeiras(cadeiras,data,config)
-        #escreve no ficheiro data
+        # eliminar jsons que ja nao existem na config WORKING TESTED
+        del_unexistent_json(data, cadeiras)
+        # atualiza cadeiras e adiciona novas cadeiras WORKING - NOT FULL TESTED
+        data = update_add_cadeiras(cadeiras, data, config)
+        # escreve no ficheiro data
         record_data(data)
